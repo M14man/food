bb9d05e198e6b83ab62a424b034b8723bf4d3314
Dmyto_Turovskiy <turovskydo@gmail.com>
2022-03-19 12:39:38 +0200

    Babel is working

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './js/script.js',
   output: {
     filename: 'bundle.js',
@@ -13,5 +13,22 @@ module.exports = {
 
   devtool: "source-map",
 
-  module: {}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/, // тут ми находимо всі js файли
+        exclude: /(node_modules|bower_components)/, // тут ми вказуэмо якы модулі виключаємо
+        use: {
+          loader: 'babel-loader', // ця технологія зв'язує webpack з babel і щоб він працювава установлюємо цей npm (npm i --save-dev babel-loader)
+          options: {
+            presets: [['@babel/preset-env', { // найпоширеніший пресет який підходить майже під всі проекти
+                debug: true,
+                corejs: 3, // core-js це бібліотека для підключення всіх можливих поліфілів (npm i --save-dev core-js)
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
